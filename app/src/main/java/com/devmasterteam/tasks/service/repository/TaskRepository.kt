0e222680b32d9ba8e83ce7226c717feafc6299f7
SilasBorges <bsilas871@hotmail.com>
2023-01-31 20:45:27 -0300

    Finish project

---
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -0,0 +1,59 @@
+package com.devmasterteam.tasks.service.repository
+
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.TaskModel
+import com.devmasterteam.tasks.service.repository.remote.PriorityService
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.devmasterteam.tasks.service.repository.remote.TaskService
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class TaskRepository(context: Context) : BaseRepository(context) {
+
+    private val remote = RetrofitClient.getService(TaskService::class.java)
+
+    fun list(listener: APIListener<List<TaskModel>>) {
+        executeCall(remote.list(), listener)
+    }
+
+    fun listNext(listener: APIListener<List<TaskModel>>) {
+        executeCall(remote.listNext(), listener)
+    }
+
+    fun listOverdue(listener: APIListener<List<TaskModel>>) {
+        executeCall(remote.listOverdue(), listener)
+    }
+
+    fun create(task: TaskModel, listener: APIListener<Boolean>) {
+        val call = remote.create(task.priorityId, task.description, task.dueDate, task.complete)
+        executeCall(call, listener)
+    }
+
+    fun update(task: TaskModel, listener: APIListener<Boolean>) {
+        val call =
+            remote.update(task.id, task.priorityId, task.description, task.dueDate, task.complete)
+        executeCall(call, listener)
+    }
+
+    fun delete(id: Int, listener: APIListener<Boolean>){
+        val call = remote.delete(id)
+        executeCall(call, listener)
+    }
+
+    fun load(id: Int, listener: APIListener<TaskModel>) {
+        executeCall(remote.load(id), listener)
+    }
+
+    fun complete(id: Int, listener: APIListener<Boolean>){
+        executeCall(remote.complete(id), listener)
+    }
+
+    fun undo(id: Int, listener: APIListener<Boolean>){
+        executeCall(remote.undo(id), listener)
+    }
+}
---
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -0,0 +1,81 @@
+package com.devmasterteam.tasks.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.PersonModel
+import com.devmasterteam.tasks.service.model.PriorityModel
+import com.devmasterteam.tasks.service.model.ValidationModel
+import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.PriorityRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.devmasterteam.tasks.view.MainActivity
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val personRepository = PersonRepository(application.applicationContext)
+    private val priorityRepository = PriorityRepository(application.applicationContext)
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
+
+    private val _login = MutableLiveData<ValidationModel>()
+    val login: LiveData<ValidationModel> = _login
+
+    private val _verifyLogged = MutableLiveData<Boolean>()
+    val verifyLogged: LiveData<Boolean> = _verifyLogged
+
+    /**
+     * Faz login usando API
+     */
+    fun doLogin(email: String, password: String) {
+        personRepository.login(email, password, object: APIListener<PersonModel>{
+            override fun onSuccess(result: PersonModel) {
+                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
+
+                RetrofitClient.addHeaders(result.token, result.personKey)
+
+                _login.value = ValidationModel()
+            }
+            override fun onFailure(message: String) {
+                _login.value = ValidationModel(message)
+            }
+        })
+    }
+
+    /**
+     * Verifica se usuário está logado
+     */
+    fun verifyLoggedUser() {
+        val token = securityPreferences.get(TaskConstants.SHARED.TOKEN_KEY)
+        val person = securityPreferences.get(TaskConstants.SHARED.PERSON_KEY)
+
+        RetrofitClient.addHeaders(token, person)
+
+        val logged = (token != "" && person != "")
+        _verifyLogged.value = logged
+
+        if(!logged){
+            priorityRepository.list(object: APIListener<List<PriorityModel>>{
+                override fun onSuccess(result: List<PriorityModel>) {
+                    val s = ""
+                    priorityRepository.save(result)
+                }
+
+                override fun onFailure(mesage: String) {
+                    val s = ""
+                }
+
+            })
+        }
+    }
+}
